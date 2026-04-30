6a53edc4cc76638955c3823a8134e12262bdef28
Alejandro Molina <jhon.jojoa@campusucc.edu.co>
2026-04-30 12:30:59 -0500

    Agregar vista de promedio general

--- calificaciones_nombre_estudiantes/views.py
+++ calificaciones_nombre_estudiantes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.db.models import Avg
 
-# Create your views here.
+from .models import Calificacion
+
+
+def promedio_general(request):
+    promedio = Calificacion.objects.all().aggregate(Avg('promedio'))['promedio__avg']
+    return render(request, 'calificaciones/promedio.html', {'promedio': promedio})

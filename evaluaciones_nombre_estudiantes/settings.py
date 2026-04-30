b2a8bdee54e1ae4b1f973eb6fffde5b1cd95bb07
EstebanR26 <david.ordonezren@campusucc.edu.co>
2026-04-30 13:09:00 -0500

    Add authentication views, login/registration templates, base layout and auth redirects

--- evaluaciones_nombre_estudiantes/settings.py
+++ evaluaciones_nombre_estudiantes/settings.py
@@ -69,6 +69,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'evaluaciones_nombre_estudiantes.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
